ce69af8a22306033aa971fc7e17f318f5a5dc30b
glasista <ruszala.rafal@gmail.com>
2019-01-05 12:46:15 +0100

    Adding EF Core

---
+++ TastyPieShop/Models/PieRepositorycs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TastyPieShop.Models
+{
+    public class PieRepositorycs:IPieRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public PieRepositorycs(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            return _appDbContext.Pies;
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return _appDbContext.Pies.FirstOrDefault(p => p.Id == pieId);
+        }
+    }
+}
--- TastyPieShop/Startup.cs
+++ TastyPieShop/Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TastyPieShop.Models;
 
@@ -12,11 +14,15 @@ namespace TastyPieShop
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IPieRepository, MockPieRepository>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<IPieRepository, PieRepository>();
 
 
             services.AddMvc();
