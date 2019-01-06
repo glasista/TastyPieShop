e901646732abfbc7b73a3f4aca37537189a0e250
glasista <ruszala.rafal@gmail.com>
2019-01-06 23:34:59 +0100

    Adding navigation

--- TastyPieShop/Controllers/HomeController.cs
+++ TastyPieShop/Controllers/HomeController.cs
@@ -34,5 +34,17 @@ namespace TastyPieShop.Controllers
 
             return View(homeViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+
+            var pie = _pieRepository.GetPieById(id);
+
+            if (pie == null)
+                return NotFound();
+
+
+            return View(pie);
+        }
     }
 }
--- TastyPieShop/Startup.cs
+++ TastyPieShop/Startup.cs
@@ -39,7 +39,14 @@ namespace TastyPieShop
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                    );
+            }
+            );
         }
     }
 }
